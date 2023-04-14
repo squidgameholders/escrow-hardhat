ad60bd845f21a77fec7b398f6632be891c49e522
SQUID Game <104257784+squidgameholders@users.noreply.github.com>
2023-04-14 23:36:53 +0800

    yeah

--- app/src/App.js
+++ app/src/App.js
@@ -29,9 +29,12 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
-    const escrowContract = await deploy(signer, arbiter, beneficiary, value);
+    // const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const value = ethers.utils.parseEther(
+      document.getElementById('ether').value
+    );
 
+    const escrowContract = await deploy(signer, arbiter, beneficiary, value);
 
     const escrow = {
       address: escrowContract.address,
@@ -55,26 +58,31 @@ function App() {
 
   return (
     <>
-      <div className="contract">
+      <div className='contract'>
         <h1> New Contract </h1>
         <label>
           Arbiter Address
-          <input type="text" id="arbiter" />
+          <input type='text' id='arbiter' />
         </label>
 
         <label>
           Beneficiary Address
-          <input type="text" id="beneficiary" />
+          <input type='text' id='beneficiary' />
         </label>
 
-        <label>
+        {/* <label>
           Deposit Amount (in Wei)
-          <input type="text" id="wei" />
+          <input type='text' id='wei' />
+        </label> */}
+
+        <label>
+          Deposit Amount (in Ether)
+          <input type='text' id='ether' />
         </label>
 
         <div
-          className="button"
-          id="deploy"
+          className='button'
+          id='deploy'
           onClick={(e) => {
             e.preventDefault();
 
@@ -85,10 +93,10 @@ function App() {
         </div>
       </div>
 
-      <div className="existing-contracts">
+      <div className='existing-contracts'>
         <h1> Existing Contracts </h1>
 
-        <div id="container">
+        <div id='container'>
           {escrows.map((escrow) => {
             return <Escrow key={escrow.address} {...escrow} />;
           })}
